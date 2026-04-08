77d3ad8afa947c8b4ba29a0bf518541c0832eb28
Linda-Song <134561105+Linda-Song@users.noreply.github.com>
2026-04-09 08:25:05 +1000

    Update Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "./components/Banner/Banner";
 import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 import TopMovieSlide from "./components/TopMovieSlide/TopMovieSlide";
-import "./homepage.style.css";
+import "./Homepage.style.css";
 import UpcomingMovieSlide from "./components/UpcomingMovieSlide/UpcomingMovieSlide";
 // 1. 배너 => popular 영화 --> 첫번째 아이템 보여주기
 // 2. popular movie
